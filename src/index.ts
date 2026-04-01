623c6e66a4afb59ae6b15b11c34f398f967d9d13
Reno Raksi <raksi@Renos-MacBook-Pro.local>
2026-03-31 19:04:52 -0500

    feat(cli): wire indicator command into CLI entry point

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { Command } from "commander";
+import { makeIndicatorCommand } from "./commands/indicator.js";
 
 const program = new Command();
 
-program.name('livefolio').description('Livefolio CLI').version('0.0.1');
+program.name("livefolio").description("Livefolio CLI").version("0.0.1");
+
+program.addCommand(makeIndicatorCommand());
 
 program.parse();
